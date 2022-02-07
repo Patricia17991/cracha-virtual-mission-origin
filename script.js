3b506cd79bc4d5268f6db4456097ccaaa825c2bb
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-02-07 15:36:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 /* a variável let = muda o valor e a const não muda o valor. Não tem como criar uma variável sem const ou let*/
 
 /*o JS vai gerar o link e por isso vamos colocar na const só o usuario */
-const github = "jakeliny";
-const youtube = "jakelinygracielly";
-const facebook = "maykbrito";
-const instagram = "jakeliny.gracielly";
-const twitter = "jakelinytec";
+const links = {
+
+     github : "jakeliny",
+     youtube : "jakelinygracielly",
+     facebook : "maykbrito",
+     instagram : "jakeliny.gracielly",
+     twitter : "jakelinytec",
+}
 
 /*função é um tipo de dado estruturado(forma de agrupar comando do JS) */
 function showMeSomething(){
