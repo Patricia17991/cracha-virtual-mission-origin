5876837cd0bd50cbba08eaaa2ea2bee0f8146405
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-02-07 16:42:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ const linksSocialMedia = {
 function changeSocialMediaLinks(){
       /*vamos usar o DOM aqui */
     document.getElementById('userName').textContent = 'Patricia'; /*pegando o elemento pelo id que esta lá no html, o textContent é p/ mudar o valor recebido anteriormente */
+     
+      userName.textcontent = 'Marcelo' /*faz o mesmo da função acima */
 }
  
 changeSocialMediaLinks();
