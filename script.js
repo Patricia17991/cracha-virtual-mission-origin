239f1cba0294b42061b75d5670ba096126dc34e1
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-02-07 15:33:01 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+/* a variável let = muda o valor e a const não muda o valor. Não tem como criar uma variável sem const ou let*/
+
+/*o JS vai gerar o link e por isso vamos colocar na const só o usuario */
+const github = "jakeliny";
+const youtube = "jakelinygracielly";
+const facebook = "maykbrito";
+const instagram = "jakeliny.gracielly";
+const twitter = "jakelinytec";
+
+/*função é um tipo de dado estruturado(forma de agrupar comando do JS) */
+function showMeSomething(){
+    alert('olá');
+
+};
+
+/*chamando a função */
+showMeSomething();
