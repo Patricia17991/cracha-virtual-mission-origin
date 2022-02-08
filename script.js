0a8f4224d03b102af907faad186530dbffafad8e
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-02-08 14:45:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,33 @@
-//a variável let = muda o valor e a const não muda o valor. Não tem como criar uma variável sem const ou let
-
-
-
-//o JS vai gerar o link e por isso vamos colocar na const só o usuario 
 const LinksSocialMedia = {
-    //para mudar as informações basta mudar essas aqui
-
-     github : "jakeliny",
-     youtube : "jakelinygracielly",
-     facebook : "maykbrito",
-     instagram : "jakeliny.gracielly",
-     twitter : "jakelinytec"
+  github: 'jakeliny',
+  youtube: 'jakelinygracielly',
+  twitch: 'jakeliny',
+  instagram: 'jakeliny.gracielly',
+  twitter: 'jakelinytec'
 }
 
-//função que muda o valor dos dados 
-function changeSocialMediaLinks(){
-      /*vamos usar o DOM aqui */
-     
-      for(let li of socialLinks.children ){ //o children vai pegar todos os filhos do id socialLinks um de cada vez
-      const social = li.getAttribute('class') //dentro da const  estou pegando a classe da li
-           
-            li.children[0].href =`https://${social}.com/${LinksSocialMedia[social]}` //passando o novo conteúdo do href.
-        
-        
-      }
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
 
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
 }
- 
-changeSocialMediaLinks();
 
-//função que pega as informações do github e trás p/ aqui p/ depois substituirmos no html.
-function getGitHubProfileInfos(){
-     //definir url da api do github
-     const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+changeSocialMediaLinks()
 
-     //chegando na url e pegando o que ela responder, no caso o JSON (usando o comando fetch)
-    fetch(url)
-     .then(response => response.json()) 
-     .then(data => {
-         userName.textContent = data.name
-         userBio.textContent = data.bio
-         userLink.href =  data.html_url
-         userPhoto.src = data.avatar_url
-         userLogin.textContent = data.login
-         
-     }) //armazena em json
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
 
-     }) //armazena em json
-}
-    
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
 }
 
 getGitHubProfileInfos()
-
-//promise no js é um recurso que usamos p/ pegar respostas de alguma coisa (.then())
-
-//podemos criar quantos thens forem preciso
